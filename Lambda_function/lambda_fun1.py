e90e449a1131bef79f865ad7b68ce2c1609e0627
khursed1 <khursednew@gmail.com>
2026-08-09 20:25:50 +0530

    lambda function

--- Lambda_function/lambda_fun1.py
+++ Lambda_function/lambda_fun1.py
@@ -1,4 +1,21 @@
-square=lambda x:x*x
+square = lambda x: x * x
 print(square(5))
 
 # lambda input:output
+
+# multiple inputs
+
+add = lambda a, b: a + b
+print(add(1, 2))
+
+# lambda with dictionary
+# suppose
+data = {
+    "name": "khursed",
+    "age": 23,
+}
+
+# lambda x:x['key']
+# you can write
+get_data = lambda x: (x["name"], x["age"])
+print(get_data(data))
